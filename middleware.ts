9c556f61bbd727cacf52ac5db7421a45ccbd3ced
Hiroko Nakano <hiro@flak.jp>
2024-02-15 17:15:25 +0900

    [fix] Basic認証内容変更

--- middleware.ts
+++ middleware.ts
@@ -1,36 +1,24 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-export const config = {
-  matcher: ['/:path*'],
-};
-
 export function middleware(req: NextRequest) {
-  const isLocalDevelopment = process.env.NODE_ENV === 'development';
+  const basicAuth = req.headers.get('authorization');
+  const url = req.nextUrl;
 
-  if (isLocalDevelopment || !process.env.AUTH_USERNAME || !process.env.AUTH_PASSWORD) {
+  const AUTH_USERNAME = process.env.AUTH_USERNAME;
+  const AUTH_PASSWORD = process.env.AUTH_PASSWORD;
+
+  if (!AUTH_USERNAME || !AUTH_PASSWORD) {
     return NextResponse.next();
   }
 
-  const basicAuth = req.headers.get('authorization');
-  if (!basicAuth) {
-    return new Response('Authentication required', {
-      headers: {
-        'WWW-Authenticate': 'Basic realm="Secure Area"',
-      },
-      status: 401,
-    });
-  }
-
-  try {
+  if (basicAuth) {
     const authValue = basicAuth.split(' ')[1];
-    const [user, pwd] = atob(authValue).split(':');
+    const [user, pwd] = Buffer.from(authValue, 'base64').toString().split(':');
 
-    if (user === process.env.AUTH_USERNAME && pwd === process.env.AUTH_PASSWORD) {
+    if (user === AUTH_USERNAME && pwd === AUTH_PASSWORD) {
       return NextResponse.next();
     }
-  } catch (e) {
-    return new Response('Invalid Authentication', { status: 400 });
   }
 
-  return new Response('Unauthorized', { status: 401 });
+  return NextResponse.rewrite(url);
 }
