c45a4fabebbde5b26abf8d279c9189370e197eb5
Hiroko Nakano <hiro@flak.jp>
2024-02-15 17:36:51 +0900

    [add] App routerの場合はroute.tsも必要らしい

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,20 @@
-import { NextRequest, NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
 
-export function middleware(req: NextRequest) {
+function middleware(req: NextRequest): NextResponse {
   const basicAuth = req.headers.get('authorization');
   const url = req.nextUrl;
-
-  const AUTH_USERNAME = process.env.AUTH_USERNAME;
-  const AUTH_PASSWORD = process.env.AUTH_PASSWORD;
-
-  if (!AUTH_USERNAME || !AUTH_PASSWORD) {
-    return NextResponse.next();
-  }
-
   if (basicAuth) {
-    const authValue = basicAuth.split(' ')[1];
-    const [user, pwd] = Buffer.from(authValue, 'base64').toString().split(':');
+    const authValue = basicAuth.split(' ')[1] ?? '';
+    const [user, pwd] = atob(authValue).split(':');
 
-    if (user === AUTH_USERNAME && pwd === AUTH_PASSWORD) {
+    if (user === process.env.AUTH_USERNAME && pwd === process.env.AUTH_PASSWORD) {
       return NextResponse.next();
     }
   }
+  url.pathname = '/api/basic-auth';
 
   return NextResponse.rewrite(url);
 }
+
+export { middleware };
